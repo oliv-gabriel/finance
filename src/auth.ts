b3329f49cc9a0078611c06717e5ec000c21b26b2
gab <gabrielgamebr102@gmail.com>
2026-09-15 21:21:57 -0400

    fix: capturar link da vercel automaticamente

--- src/auth.ts
+++ src/auth.ts
@@ -16,9 +16,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Passkey({
       relayingParty: {
-        id: process.env.AUTH_URL ? new URL(process.env.AUTH_URL).hostname : "localhost",
+        id: process.env.AUTH_URL ? new URL(process.env.AUTH_URL).hostname : (process.env.VERCEL_PROJECT_PRODUCTION_URL || process.env.VERCEL_URL || "localhost"),
         name: "Finance App",
-        origin: process.env.AUTH_URL || "http://localhost:3000",
+        origin: process.env.AUTH_URL ? process.env.AUTH_URL : (process.env.VERCEL_PROJECT_PRODUCTION_URL || process.env.VERCEL_URL ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL || process.env.VERCEL_URL}` : "http://localhost:3000"),
       }
     }),
     Credentials({
